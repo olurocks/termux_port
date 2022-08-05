f2f2b1ddc79ef4c4b22ec1aa5ab2812dd01b9ca8
olurocks <59121110+olurocks@users.noreply.github.com>
2022-08-05 01:59:02 +0100

    Update portman.py

--- portman.py
+++ portman.py
@@ -4,7 +4,7 @@ import subprocess
 import json
 import re
 
-
+daily_limit = int(input("enter your expected daily expenditure")
 def check_time(init_time):
     time_t = datetime.strptime(init_time, '%Y-%m-%d %H:%M:%S')
     if time_t.date() < datetime.today().date():
@@ -44,7 +44,6 @@ def get_specific_messages():
 #check for messages that are from banks(i.e alerts)
 def access_alerts():
     banks = "UNIONBANK UBA ZENITHBANK ".lower().split()
-#    print(banks)
     alerts = []
     day_messages = get_specific_messages()
     for i in day_messages:
@@ -52,13 +51,12 @@ def access_alerts():
             alerts.append(i)    
     return alerts   
 
-#'''check if alert is credit or debit
+#'''check if alert is credit or debit and compute the total cr or dr amt
 def check_alerts():
     alerts = access_alerts()
     cred = []
     deb =[]
     for al in alerts:
-        # if alert is credit,extract credit amount
         if "cr:" in al["body"].lower() or "txn: credit"  in al["body"].lower():
             cr = re.compile(r'(cr|amt)\w*.?.?\w*.?.?ngn.?\d{1,3}.?\d{1,3}.?\d{1,3}.?\d{1,3}.?\d{1,3}')
             extract_figs = re.compile(r'\d{1,3}.?\d{1,3}.?\d{1,3}.?')
@@ -68,10 +66,7 @@ def check_alerts():
             for i in credit_amount:
                 b = float(i.group().replace(",",""))
                 cred.append(b)
-
-
-        # if alert is debit, extract debit amount
-        if "dr:" in al["body"].lower() or "txn: debit" in al['body'].lower() or "dt:"in al['body'].lower():
+        elif "dr:" in al["body"].lower() or "txn: debit" in al['body'].lower() or "dt:"in al['body'].lower():
             de = re.compile(r'(d|amt)\w*.?.?\w*.?.?ngn.?\d{1,3}.?\d{1,3}.?\d{1,3}.?\d{1,3}')
             extract_figs = re.compile(r'\d{1,3}.?\d{1,3}.?\d{1,3}.?')
             debit = de.finditer(al['body'].lower())
@@ -79,40 +74,49 @@ def check_alerts():
             for i in debit:
                 debit_amount = extract_figs.finditer(i.group())
                 for i in debit_amount:
-                    # b = float(i.group().replace(",",""))
                     b = float(i.group().replace(",",""))
                     deb.append(b)
-    
-    # find the sum of total credits against total debits 
     total_credit = sum(cred)
     total_debit = sum(deb)
+    os.system(f"termux-toast -b green -c blue -g top -s your total amount received today is : \#{total_credit}")
+    os.system(f"termux-toast -b green -c blue -g top -s your total amount spent so far today is : \#{total_debit}")
     return total_credit,total_debit
 
-    print("your total amount received today is :",sum(cred))
-    print("your total amount spent so far today is :",sum(deb))
 
 
 
 
-# calculate the percentage of debit against credit
 def notif():
     ea = check_alerts()
     credit = ea[0]
     debit = ea[1]
-    percent = debit/credit * 100
-    print(f"your percentage expenditure is {percent}%" )
-
+    while datetime.today().date():
+        limit = daily_limit
+        break
+    if debit == 0:
+       notification = os.system("termux-toast -b red -c cyan -g top you have zero expenditures as of today")
+    else:
+        dl_percent = debit / limit * 100
+        if dl_percent >= 10:
+            notification = os.system("termux-toast -b teal -c red -g top  warning: you have spent at least 10% of your daily expenditure limit")
+        elif dl_percent >= 20:
+            notification = os.system("termux-toast -b teal -c red -g top  warning: you have spent at least 20% of your daily expenditure limit")
+        elif dl_percent >= 30:
+            notification = os.system("termux-toast -b teal -c red -g top -s warning: you have spent at least 30% of your daily expenditure limit")
+        elif dl_percent >= 50:
+            notification = os.system("termux-toast -b teal -c red -g top -s warning: you have spent at least 50% of your daily expenditure limit")
+        elif dl_percent >= 75:
+            notification = os.system("termux-toast -b teal -c red -g top -s warning: you have spent at least 75% of your daily expenditure limit")
+        elif dl_percent == 100:
+            notification = os.system("termux-toast -b teal -c red -g top -s warning: you have spent at least 100% of your daily expenditure limit")
+        elif dl_percent > 100:
+            notification = os.system(f"termux-toast -b teal -c red -g top -s warning: you have spent {round((dl_percent - 100),2)}% more than your daily expenditure limit /n which is {debit - limit}% more than {limit}")
+        notification = os.system(f"termux-toast -b teal -c green -g top -s you have spent {round(dl_percent,2)}% of your daily limit \n")
     
-# if the percentage of debit to credit  >= 20%: send a mild notification
-    if percent < 20:
-        return print(f"your percentage expenditure is {percent}%")
-# if the percentage of debit to credit  >= 50%: send a warning
-    elif percent >=50:
-        return print(f"your percentage expenditure is {percent}%\n\n warning: you have spent at least 50% of your daily income so far")
-# if the percentage of debit to credit  >= 70%: send a wild warning
-    elif percent >= 70:
-        return print(f"your percentage expenditure is {percent}% oi watch it, you have spent at least 70% of your daily income so far")
+    if debit > credit:
+        notification = os.system(f"termux-toast -b teal -c red -s -g top you have spent \#{debit - credit} more than your daily income")
+    if debit < limit:
+        notification = os.system(f"termux-toast -b teal -c brown -g top -s you have \#{limit - debit} left of your daily expenditure for the day")
+    return notification
 
-# if the percentage of debit to credit  >= 100%: "send spent more than income warning"  '''
-    elif percent >= 100: 
-        return print(f"your percentage expenditure is {percent}% are you nuts!, you have spent at least all of your daily income so far")
+notif()
